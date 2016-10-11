eb0711dd847ee04faea87962a5e842213d1121ac
pratik1998patel <pratik1998patel@gmail.com>
2016-10-11 14:03:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/artical-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/artical-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'artical-two.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/artical-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'artical-three.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
